1765f1cb0d195ea2471c8a13b597b06da08c8d96
Jeffrey Jin <jinjeff2@gmail.com>
2021-03-27 00:07:53 -0400

    added route to return all location sites

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
 const express = require( "express" );
 const app = express();
 const port = 8000; // default port to listen
+const prisma = new PrismaClient();
 
 // start the Express server
 app.listen(port, () => {
     console.log( `Server started at http://localhost:${ port }` );
-} );
+});
+
+app.get('/listAllLocations', async (req:any, res:any) => {
+    const locations = await prisma.mutualAid.findMany()
+    res.json(locations)
+})
