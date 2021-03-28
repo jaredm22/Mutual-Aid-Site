161dbccccb212ce904e3d99b936ac2bbeb439e13
Jeffrey Jin <jinjeff2@gmail.com>
2021-03-28 17:47:14 -0400

    fixed server routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
+const { PrismaClient } = require("@prisma/client");
 const express = require( "express" );
 const app = express();
 const port = 8000; // default port to listen
@@ -10,7 +9,18 @@ app.listen(port, () => {
     console.log( `Server started at http://localhost:${ port }` );
 });
 
-app.get('/listAllLocations', async (req:any, res:any) => {
+app.get('/listAllLocations', async (req, res) => {
     const locations = await prisma.mutualAid.findMany()
+    res.json(locations)
+})
+
+app.get('/locationInfo/:zip', async (req, res) => {
+    const locations = await prisma.mutualAid.findMany({
+        where: {
+            zip: {
+                equals: req.params.zip
+            }
+        }
+    })
     res.json(locations)
 })
