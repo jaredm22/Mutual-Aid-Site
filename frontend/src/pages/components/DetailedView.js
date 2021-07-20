a4b8e62f63250c5c60cb2e0d0124b503183728bf
Jeffrey Jin <jinjeff2@gmail.com>
2021-07-20 17:27:15 -0400

    added pagination and display of individual results

---
+++ frontend/src/pages/components/DetailedView.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import temp from '../../images/temp.png';
+import './detailedview.css'
+
+class DetailedView extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: this.props.location.name,
+            days: this.props.location.days,
+            times: this.props.location.times,
+            phone: this.props.location.phone,
+            address_one: this.props.location.address_one,
+            address_two: this.props.location.address_two,
+            city: this.props.location.city,
+            state: this.props.location.state,
+            zip: this.props.location.zip,
+            email: this.props.location.email,
+            website: this.props.location.website
+
+        }
+        this.getLocationDesc = this.getLocationDesc.bind(this);
+        this.onBackClick = this.onBackClick.bind(this);
+    }
+
+    getLocationDesc() {
+        // Handle contact information, day availability, time availabilities         
+        var days, times, phone, email, website = ""
+        if (this.state.days.length == 0) {
+            days = "Not available"
+        } else {
+            for (let x = 0; x < this.state.days.length; x++) {
+                days += this.state.days[x] + ", "
+            }
+            days = days.substring(0,days.length-2)
+        }
+
+        if (this.state.times.length == 0) {
+            times = "Not available"
+        } else {
+            for (let x = 0; x < this.state.times.length; x++) {
+                times += this.state.times[x] + ", "
+            }
+            times = times.substring(0,times.length-2)
+        }
+    
+        if (this.state.phone == "") {
+            phone = "Not available"
+        } else {
+            phone = this.state.phone
+        }
+        if (this.state.email == "") {
+            email = "Not available"
+        } else {
+            email = this.state.email
+        }
+        if (this.state.website == "") {
+            website = "Not available"
+        } else {
+            website = this.state.website
+        }
+
+
+        // Handle address information and display corresponding results
+        if (this.state.address_one === "") {
+            return (
+                <div className="location-description">
+                    <div>Distribution Days: {days}</div>
+                    <div>Distribution Time: {times}</div>
+                    <div>Contact: #{phone}</div>
+                    <div>Email: {email}</div>
+                    <div>Address: Not Available</div>
+                    <div><a href={website} target="_blank">Website</a></div>
+                </div>
+            )
+        } else if (this.state.address_two === "") {
+            return(
+                <div className="location-description">
+                    <div>Distribution Days: {days}</div>
+                    <div>Distribution Time: {times}</div>
+                    <div>Contact: #{phone}</div>
+                    <div>Email: #{email}</div>
+                    <div>{this.state.address_one}</div>
+                    <div>{this.state.city}, {this.state.state} {this.state.zip}</div>
+                    <div><a href={website} target="_blank">Website</a></div>
+                </div>
+            )
+        }
+    }
+
+    onBackClick() {
+        this.props.parentCallback()
+    }
+
+    render() {
+        return(
+            <div className="detailed-view">
+                <div className="header">
+                    <button className="back-button" onClick={this.onBackClick}>Back</button>
+                    <div className="location-name">{this.state.name}</div>
+                </div>
+                <div className="image"><img src={temp} alt="location image"/></div>
+                {this.getLocationDesc()}
+            </div>
+        )
+    }
+}
+
+export default DetailedView
--- frontend/src/pages/components/SearchResults.js
+++ frontend/src/pages/components/SearchResults.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios';
 import temp from '../../images/temp.png';
 import './searchresults.css'
+import DetailedView from './DetailedView';
+
 
 class SearchResults extends React.Component {
 
@@ -9,15 +11,28 @@ class SearchResults extends React.Component {
         super(props)
         this.state = {
             locations: [], //receive from parent when search bar is completed
-            loading: true
+            loading: true,
+            locationsPerPage: 5,
+            currentPage: 1,
+            maxPage: -1,
+            prevDisabled: true,
+            nextDisabled: false,
+            displayResults: true,
+            locationDisplay: {}
         }
         this.getLocations = this.getLocations.bind(this);
+        this.paginate = this.paginate.bind(this);
+        this.onNextClick = this.onNextClick.bind(this);
+        this.onPrevClick = this.onPrevClick.bind(this);
+        this.onViewClick = this.onViewClick.bind(this);
+        this.parentCallback = this.parentCallback.bind(this);
     }
 
     componentDidMount() {
         this.getLocations()
     }
 
+    // Get a list of locations (removed when search bar is functioning)
     getLocations() {
         const options = {
             url: 'http://localhost:5000/listAllLocations',
@@ -29,38 +44,94 @@ class SearchResults extends React.Component {
         axios(options)
         .then( res => {
             var locations = res.data
-            this.setState({locations: locations, loading: false})
+            var currentPageIndex = 1
+            var counter = 0
+            for (let x = 0; x< locations.length; x++) {
+                if (counter-(this.state.locationsPerPage) === 0) {
+                    currentPageIndex += 1
+                    counter = 0
+                }
+                locations[x]['page'] = currentPageIndex
+                counter++;
+            }
+            if (currentPageIndex === 1) {
+                this.setState({nextDisabled: true})
+            }
+            this.setState({locations: locations, loading: false, maxPage: currentPageIndex})
         })
         .catch(err => {
             console.log(err)
         })
     }
 
+    // Adjust page when next button is clicked
+    onNextClick() {
+        if (this.state.currentPage+1 === this.state.maxPage) {
+            this.setState({nextDisabled: true})
+        }
+        this.setState({currentPage: this.state.currentPage+1, prevDisabled: false})
+    }
+
+    // Adjust page when previous button is clicked
+    onPrevClick() {
+        if (this.state.currentPage-1 === 1) {
+            this.setState({prevDisabled: true})
+        }
+        this.setState({currentPage: this.state.currentPage-1, nextDisabled: false})
+    }
+
+    // Change display to individual display
+    onViewClick(location) {
+        this.setState({displayResults: false, locationDisplay: location})
+        console.log(location)
+    }
+
+    // handles functionality when back button is pressed
+    parentCallback() {
+        this.setState({displayResults: true})
+    }
+
+    // split locations into individual groups based on locations
+    paginate() {
+        return(
+            this.state.locations.map((location, index) => {
+                if (location.page === this.state.currentPage) {
+                    return (
+                        <div className="location" key={location.name}>
+                            <div className="location-index">{index+1}</div>
+                            <div className="location-desc">
+                                <div className="location-info title">{location.name}</div>
+                                <div className="location-info">Distribution Days: Wednesday</div>
+                                <div className="location-info">Distribution Time: Varies</div>
+                                {location.phone == '' ? 
+                                <div className="location-info">Contact: Not Available</div> :
+                                <div className="location-info">Contact: #{location.phone}</div>
+                                }
+                            </div>
+                            <div className="location-button">
+                                <button className="view-button" onClick={()=>this.onViewClick(location)}>View</button>
+                            </div>
+                        </div>
+                    )
+                }
+            }
+        ))
+    }
 
     render() {
         return(
             <React.Fragment>
-                {this.state.loading ? <div>Loading</div> :
+                { this.state.displayResults ?
+                    this.state.loading ? <div>Loading</div> :
                     <div className="result-group">
-                        {this.state.locations.map((location, index) => {
-                            return (
-                            <div className="location">
-                                <div className="location-index">{index+1}</div>
-                                <div className="location-desc">
-                                    <div className="location-info title">{location.name}</div>
-                                    <div className="location-info">Distribution Days: Wednesday</div>
-                                    <div className="location-info">Distribution Time: Varies</div>
-                                    {location.phone == '' ? 
-                                    <div className="location-info">Contact: Not Available</div> :
-                                    <div className="location-info">Contact: #{location.phone}</div>
-                                    }
-                                </div>
-                                <div className="location-button">
-                                </div>
-                            </div>
-                            )
-                        })}
+                        {this.paginate()}
+                        <div className="button-group">
+                            <button className="page-button prev" disabled={this.state.prevDisabled} onClick={this.onPrevClick}>Prev</button>
+                            <button className="page-button next" disabled={this.state.nextDisabled} onClick={this.onNextClick}>Next</button>
+                        </div>
                     </div>
+                    :
+                    <DetailedView parentCallback={this.parentCallback} location={this.state.locationDisplay}/>
                 }
             </React.Fragment>
         )
